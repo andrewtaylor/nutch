41f43073eb85e8a1ab14d117002e67a89e883584
Andrew Taylor <andrew@brekka.co.uk>
2013-06-28 20:41:51 +0000

    Use httpclient 4

--- src/java/org/apache/nutch/indexer/solr/SolrUtils.java
+++ src/java/org/apache/nutch/indexer/solr/SolrUtils.java
@@ -16,61 +16,101 @@
  */
 package org.apache.nutch.indexer.solr;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.params.HttpClientParams;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
-
-import java.net.MalformedURLException;
 
 public class SolrUtils {
 
-  public static Logger LOG = LoggerFactory.getLogger(SolrIndexer.class);
+	public static Logger LOG = LoggerFactory.getLogger(SolrUtils.class);
 
-  public static CommonsHttpSolrServer getCommonsHttpSolrServer(JobConf job) throws MalformedURLException {
-    HttpClient client=new HttpClient();
+	public static SolrServer getCommonsHttpSolrServer(JobConf job)
+			throws MalformedURLException {
+		URL url = new URL(job.get(SolrConstants.SERVER_URL));
 
-    // Check for username/password
-    if (job.getBoolean(SolrConstants.USE_AUTH, false)) {
-      String username = job.get(SolrConstants.USERNAME);
+		DefaultHttpClient client = new DefaultHttpClient();
 
-      LOG.info("Authenticating as: " + username);
+		// Check for username/password
+		if (job.getBoolean(SolrConstants.USE_AUTH, false)) {
+			String username = job.get(SolrConstants.USERNAME);
 
-      AuthScope scope = new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM, AuthScope.ANY_SCHEME);
+			LOG.info("Authenticating as: " + username);
 
-      client.getState().setCredentials(scope, new UsernamePasswordCredentials(username, job.get(SolrConstants.PASSWORD)));
+			client.addRequestInterceptor(new PreemptiveAuthInterceptor(), 0);
+			client.getCredentialsProvider().setCredentials(
+					new AuthScope(url.getHost(), url.getPort()),
+					new UsernamePasswordCredentials(username, job
+							.get(SolrConstants.PASSWORD)));
+		}
 
-      HttpClientParams params = client.getParams();
-      params.setAuthenticationPreemptive(true);
+		return new HttpSolrServer(url.toString(), client);
+	}
 
-      client.setParams(params);
-    }
+	public static String stripNonCharCodepoints(String input) {
+		StringBuilder retval = new StringBuilder();
+		char ch;
 
-    return new CommonsHttpSolrServer(job.get(SolrConstants.SERVER_URL), client);
-  }
+		for (int i = 0; i < input.length(); i++) {
+			ch = input.charAt(i);
 
-  public static String stripNonCharCodepoints(String input) {
-    StringBuilder retval = new StringBuilder();
-    char ch;
+			// Strip all non-characters
+			// http://unicode.org/cldr/utility/list-unicodeset.jsp?a=[:Noncharacter_Code_Point=True:]
+			// and non-printable control characters except tabulator, new line
+			// and carriage return
+			if (ch % 0x10000 != 0xffff && // 0xffff - 0x10ffff range step
+											// 0x10000
+					ch % 0x10000 != 0xfffe && // 0xfffe - 0x10fffe range
+					(ch <= 0xfdd0 || ch >= 0xfdef) && // 0xfdd0 - 0xfdef
+					(ch > 0x1F || ch == 0x9 || ch == 0xa || ch == 0xd)) {
 
-    for (int i = 0; i < input.length(); i++) {
-      ch = input.charAt(i);
+				retval.append(ch);
+			}
+		}
 
-      // Strip all non-characters http://unicode.org/cldr/utility/list-unicodeset.jsp?a=[:Noncharacter_Code_Point=True:]
-      // and non-printable control characters except tabulator, new line and carriage return
-      if (ch % 0x10000 != 0xffff && // 0xffff - 0x10ffff range step 0x10000
-          ch % 0x10000 != 0xfffe && // 0xfffe - 0x10fffe range
-          (ch <= 0xfdd0 || ch >= 0xfdef) && // 0xfdd0 - 0xfdef
-          (ch > 0x1F || ch == 0x9 || ch == 0xa || ch == 0xd)) {
+		return retval.toString();
+	}
 
-        retval.append(ch);
-      }
-    }
+	static class PreemptiveAuthInterceptor implements HttpRequestInterceptor {
 
-    return retval.toString();
-  }
+		public void process(final HttpRequest request, final HttpContext context)
+				throws HttpException, IOException {
+			AuthState authState = (AuthState) context
+					.getAttribute(ClientContext.TARGET_AUTH_STATE);
+
+			// If no auth scheme avaialble yet, try to initialize it
+			// preemptively
+			if (authState.getAuthScheme() == null) {
+				CredentialsProvider credsProvider = (CredentialsProvider) context
+						.getAttribute(ClientContext.CREDS_PROVIDER);
+				HttpHost targetHost = (HttpHost) context
+						.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+				Credentials creds = credsProvider.getCredentials(new AuthScope(
+						targetHost.getHostName(), targetHost.getPort()));
+				if (creds == null)
+					throw new HttpException(
+							"No credentials for preemptive authentication");
+				authState.update(new BasicScheme(), creds);
+			}
+		}
+	}
 }
